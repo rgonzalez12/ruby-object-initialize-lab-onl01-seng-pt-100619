25a56f6ddf576020f5b9c050d0b48227d1b973e8
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-24 08:22:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,22 @@
 class Dog 
-  def initialize(dog_name, breed)
+  def initialize(dog_name)
     @dog = dog_name
     @breed = breed
   end
   
   def
    @dog
-   @breed
+  end
+  
+  def initialize(breed)
+    @breed = breed
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
   end
 end
